738dda5ca5e1b5023e44d2a04bfac8c9a3d04ec5
Jonydakid <jonathan.torres.salvo@gmail.com>
2020-04-10 20:33:47 -0400

    creación de spider

---
+++ scrapyApp/scrapyApp/spiders/posts_spider.py
@@ -0,0 +1,26 @@
+import scrapy
+
+
+class PostsSpider(scrapy.Spider):
+    name = "posts"
+
+    def start_requests(self):
+        urls = [
+            'https://blog.scrapinghub.com/',
+            'https://blog.scrapinghub.com/page/2',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        for post in response.css('div.post-item'):
+            yield{
+                'title': post.css('.post-header h2 a::text')[0].get(),
+                'date': post.css('.post-header a::text')[1].get(),
+                'author': post.css('.post-header a::text')[2].get(),
+                'desc': post.css('.post-content span::text').get()
+            }
+        next_page= response.css('a.next-posts-link::attr(href)').get()
+        if next_page is not None:
+            next_page=response.urljoin(next_page)
+            yield scrapy.Request(next_page,callback=self.parse)
