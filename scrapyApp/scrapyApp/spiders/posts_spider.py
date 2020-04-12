eb6f950b67f0d343a15ec90289a86228d54e826a
Jonydakid <jonathan.torres.salvo@gmail.com>
2020-04-12 12:40:28 -0400

    Avance de scrapy

--- scrapyApp/scrapyApp/spiders/posts_spider.py
+++ scrapyApp/scrapyApp/spiders/posts_spider.py
@@ -14,12 +14,17 @@ class PostsSpider(scrapy.Spider):
 
     def parse(self, response):
         for post in response.css('div.post-item'):
+            description=post.css('.post-content span::text').get()
+            if description is None:
+                description=post.css('.post-content p::text').get()
             yield{
                 'title': post.css('.post-header h2 a::text')[0].get(),
                 'date': post.css('.post-header a::text')[1].get(),
                 'author': post.css('.post-header a::text')[2].get(),
-                'desc': post.css('.post-content span::text').get()
+                'desc': description
             }
+
+            
         next_page= response.css('a.next-posts-link::attr(href)').get()
         if next_page is not None:
             next_page=response.urljoin(next_page)
